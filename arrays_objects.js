33b90f3195d4bdc5a2ea06f8fd0ace3d8ff0dbcf
Corneliousm808 <corneliousmooring@gmail.com>
2016-12-05 08:39:34 -1000

    its up

---
+++ arrays_objects.js
@@ -0,0 +1,26 @@
+/*
+
+Cornelious Mooring
+Arrays and Objects
+Dec. , 5, 2016
+
+*/
+
+// var
+// arrays
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var names = ["Senior Tan", "Zakattak", "Yolo Baggins", "Ohai", "Basic Jeff"];
+var colors = ["black", "ron burgundy", "highlighter"];
+var adj = ["smart", "special case", "yummy", "tiny whiny", "ferocious"]
+var slang = ["scrap", "kobe beef", "da kine", "li 'dat", "pau hana"]
+
+
+// objects
+
+var donut = {
+  toppings : ["sprinkles", "chocolate frosting", "o'hare air"],
+  fillings : "jelly",
+  shape : "square",
+  count : 12
+}
+
